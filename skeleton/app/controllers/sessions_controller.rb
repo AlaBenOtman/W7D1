9c4aba40abbaa36ed4e2d2777a58858dd0d794fe
akerpelm <akerpelm@u.rochester.edu>
2021-04-05 17:09:34 -0400

    finish new.html, begin work on after_initialize, sessions_controller

--- skeleton/app/controllers/sessions_controller.rb
+++ skeleton/app/controllers/sessions_controller.rb
@@ -1,12 +1,12 @@
 class SessionsController < ApplicationController
 
     def new
-
+        @user = User.new
+        render :new
     end
 
     def create
         @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
-
         if @user 
             login!(@user)
             redirect_to cats_url 
--- skeleton/app/models/user.rb
+++ skeleton/app/models/user.rb
@@ -1,11 +1,27 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#
 class User < ApplicationRecord
     
 validates :session_token, :username,  presence: true, uniqueness: true
 validates :password_digest, presence: true 
 validates :password, length: {minimum: 6, allow_nil: true }
 
+after_initialize :ensure_session_token
+
+attr_reader :password
+
+    def ensure_session_token
 
 
+    end
+    
     def reset_session_token!
         self.session_token = SecureRandom::urlsafe_base64
         self.save!
