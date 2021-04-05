28290891e5e57b8bb2567de272441df348f58076
akerpelm <akerpelm@u.rochester.edu>
2021-04-05 15:00:23 -0400

    Finish user model

--- skeleton/app/models/user.rb
+++ skeleton/app/models/user.rb
@@ -1,8 +1,34 @@
-class Cat < ApplicationRecord
+class User < ApplicationRecord
     
 validates :session_token, :username,  presence: true, uniqueness: true
 validates :password_digest, presence: true 
 validates :password, length: {minimum 6, allow_nill: true }
 
+
+
+    def reset_session_token!
+        self.session_token = SecureRandom::urlsafe_base64
+        self.save!
+        self.session_token
+    end
+
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+        @password = password
+    end
+
+    def is_password?(password)
+        password_object = BCrypt::Password.new(self.password_digest)
+        password_object.is_password?(password)
+    end
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        if user && user.is_password?(password)
+            user
+        else
+            nil
+        end
+    end
 
 end
